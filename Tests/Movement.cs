dc598f26564bfb89cc91aafbf09d8d7a77b82e08
Prozorov Peter <Prozorov.peter@gmail.com>
2022-03-11 06:33:35 +0700

    Added a test for movement.

---
+++ Tests/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using TheTurtle;
+using Xunit;
+
+namespace Tests; 
+
+public class Movement {
+    [Fact]
+    public void SomeTest() {
+        // Arrange
+        var t = new Turtle();
+        
+        // Act
+        t.MoveForward(10);
+        t.Turn(TurnDirections.Right);
+        t.MoveForward(10);
+        t.Turn(TurnDirections.Right);
+        t.MoveForward(20);
+        t.Turn(TurnDirections.Right);
+        t.MoveForward(20);
+        var state = t.whatIsMyState();
+        
+        // Assert
+        Assert.Equal(state, new TurtleState {
+            Direction = Directions.East,
+            x = 10,
+            y = -10
+        });
+    }
+}
